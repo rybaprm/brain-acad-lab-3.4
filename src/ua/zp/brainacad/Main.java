1a065af58bc6c8f7268beb650eb9d5ccbc700adb
rybaprm <59397258+rybaprm@users.noreply.github.com>
2020-08-18 13:02:18 +0300

    overflow mantissa of numbers

---
+++ src/ua/zp/brainacad/Main.java
@@ -0,0 +1,12 @@
+package ua.zp.brainacad;
+
+public class Main {
+    /**
+     * Class Main output to consol overflow mantissa of numbers
+     */
+    public static void main(String[] args) {
+        byte byteVar = 127;
+        byteVar++;
+        System.out.println("increment of max number in byte type (127++):\n" + byteVar);
+    }
+}
